73b62c1ca64063bf0931544024a2f8d2456db7a3
RaiderTs <63519514+RaiderTs@users.noreply.github.com>
2022-01-13 18:06:33 +0200

    add button

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <Button @click="increment" outlined>Click me!</Button>
+  </div>
+</template>
+
+<script>
+import Button from './components/Button.vue';
+export default {
+  name: 'App',
+  components: {
+    Button,
+  },
+  data() {
+    return {
+      amountOfClicks: 0,
+    };
+  },
+  computed: {
+    title() {
+      return `Amount of clicks ${this.amountOfClicks}`;
+    },
+  },
+  methods: {
+    increment() {
+      this.amountOfClicks += 1;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
